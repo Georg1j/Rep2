8416899c554c8c7f06ba4b29d92e7b914b595e9e
Георгий <egor2018smirnov@mail.ru>
2023-08-03 15:11:53 +0300

    7 занятие

--- test/app_test/models.py
+++ test/app_test/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Test(models.Model):
+    
+    title = models.CharField('заголовок', max_length=128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    action = models.BooleanField('торг', help_text='Поставьте галочку, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+            db_table = 'advertisements'
+            
